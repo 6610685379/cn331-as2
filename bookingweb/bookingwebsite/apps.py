64d49c529d5e915f7305ccd41601d552637082fc
6610685379 <142779621+6610685379@users.noreply.github.com>
2025-09-20 17:31:38 +0700

    commented the ready command that create superuser

--- bookingweb/bookingwebsite/apps.py
+++ bookingweb/bookingwebsite/apps.py
@@ -5,18 +5,17 @@ class BookingwebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookingwebsite'
 
-    def ready(self):
-        import os
-        from django.db.models.signals import post_migrate
-        from django.contrib.auth import get_user_model
+    # def ready(self):
+    #     from django.db.models.signals import post_migrate
+    #     from django.contrib.auth import get_user_model
 
-        def ensure_superuser(sender, **kwargs):
-            User = get_user_model()
-            u = os.getenv("DJANGO_SUPERUSER_USERNAME")
-            e = os.getenv("DJANGO_SUPERUSER_EMAIL")
-            p = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-            if u and p and not User.objects.filter(username=u).exists():
-                User.objects.create_superuser(u, e, p)
+    #     def ensure_superuser(sender, **kwargs):
+    #         User = get_user_model()
+    #         u = os.getenv("DJANGO_SUPERUSER_USERNAME")
+    #         e = os.getenv("DJANGO_SUPERUSER_EMAIL")
+    #         p = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+    #         if u and p and not User.objects.filter(username=u).exists():
+    #             User.objects.create_superuser(u, e, p)
 
-        # run AFTER migrations so auth_user exists
-        post_migrate.connect(ensure_superuser, sender=self)
+    #     # run AFTER migrations so auth_user exists
+    #     post_migrate.connect(ensure_superuser, sender=self)
