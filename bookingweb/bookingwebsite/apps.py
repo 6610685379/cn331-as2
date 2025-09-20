882c995b3cbb7388bf135a3636ba001f34923057
6610685379 <142779621+6610685379@users.noreply.github.com>
2025-09-20 16:39:03 +0700

    Update apps.py

--- bookingweb/bookingwebsite/apps.py
+++ bookingweb/bookingwebsite/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class BookingwebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookingwebsite'
+
+class BookingwebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookingwebsite'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        u = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        e = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        p = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+        if u and p and not User.objects.filter(username=u).exists():
+            User.objects.create_superuser(u, e, p)
